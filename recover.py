5d8564b4ae65a80ed7e742fdb965cb0cc9e66131
Jason R. McNeil <jason@jasonrm.net>
2017-04-03 23:38:29 -0700

    --target-acl and --delete-after-copy options, handle ctrl-c

--- recover.py
+++ recover.py
@@ -3,6 +3,7 @@ import mimetypes
 from argparse import ArgumentParser
 from configparser import ConfigParser
 from re import compile
+from signal import signal, SIGTERM, SIGINT
 
 from boto3 import Session as BotoSession
 from botocore.client import Config
@@ -69,7 +70,9 @@ parser.add_argument("--source-bucket-profile", default="default", help="Profile 
 parser.add_argument("--target-bucket-profile", default="default", help="Profile to use for target bucket")
 parser.add_argument("--dry-run", action='store_true', help="Dry Run")
 parser.add_argument("--guess-content-type", action='store_true', help="Reset the content type using best guess from key extension")
+parser.add_argument("--target-acl", default=False, help="ACL to assign to target copy")
 parser.add_argument("--key-regex", default=".*", help="Only process keys matching regex")
+parser.add_argument("--delete-after-copy", action='store_true', help="WARNING: destructive operation, will delete source object after copy")
 parser.add_argument("--ceph-conf", default="/etc/ceph/ceph.conf", help="Location of ceph.conf file")
 args = parser.parse_args()
 
@@ -79,33 +82,51 @@ source = configured_boto_client(args.source_bucket_profile)
 target = configured_boto_client(args.target_bucket_profile)
 dry_run = args.dry_run
 
+delete_after_copy = args.delete_after_copy
+
+same_bucket_name = args.source_bucket_name == args.target_bucket_name
+same_profile_name = args.source_bucket_profile == args.target_bucket_profile
+if delete_after_copy and same_bucket_name and same_profile_name:
+    logging.error("Unable to use --delete-after-copy when using the same bucket and profile for both source and target")
+    exit(1)
+
 logging.info('Source bucket marker: {}'.format(args.source_bucket_marker))
 logging.info('Source bucket : {}'.format(args.source_bucket_name))
 logging.info('Source profile: {}'.format(args.source_bucket_profile))
 logging.info('Target bucket : {}'.format(args.target_bucket_name))
 logging.info('Target profile: {}'.format(args.target_bucket_profile))
 logging.info('Key regex: {}'.format(args.key_regex))
-logging.info('Dry Run: {}'.format(args.dry_run or False))
+logging.info('Target ACL: {}'.format(args.target_acl))
+logging.info('Dry Run: {}'.format(dry_run or False))
+logging.info('Delete After Copy: {}'.format(delete_after_copy or False))
 
+logging.info('searching...')
 for key in rados.list_objects(args.key_regex):
+    reference = {
+        'Bucket': args.source_bucket_name,
+        'Key': key
+    }
+    extra_args = {
+        'MetadataDirective': 'REPLACE'
+    }
+    if args.target_acl:
+        extra_args['ACL'] = args.target_acl
+    if args.guess_content_type:
+        content_type = mimetypes.guess_type(key)[0] or ""
+        logging.info('Guessed content-type "{}" for key "{}"'.format(content_type, key))
+        extra_args['ContentType'] = content_type
     try:
-        reference = {
-            'Bucket': args.source_bucket_name,
-            'Key': key
-        }
-        extra_args = {
-            'ACL': 'private',
-            'MetadataDirective': 'REPLACE'
-        }
-        if args.guess_content_type:
-            content_type = mimetypes.guess_type(key)[0] or ""
-            logging.info('Guessed content-type "{}" for key "{}"'.format(content_type, key))
-            extra_args['ContentType'] = content_type
         if not dry_run:
             logging.info('Copying key "{}"'.format(key))
-            target.copy(reference, args.target_bucket_name, key, ExtraArgs=extra_args)
+            success = target.copy(reference, args.target_bucket_name, key, ExtraArgs=extra_args, SourceClient=source)
+            if delete_after_copy:
+                logging.info('Deleting key "{}"'.format(key))
+                source.delete_object(Bucket=args.source_bucket_name, Key=key)
         else:
             logging.info('Would copy key "{}"'.format(key))
+    except KeyboardInterrupt:
+        logging.info('Exiting…')
+        exit()
     except:
         logging.error('Failed to copy key "{}"'.format(key))
-    logging.debug('searching...')
+    logging.info('searching...')
